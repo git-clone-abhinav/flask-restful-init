c0a7aef6907f09bd97a2807a0db7535cc18b093d
git-clone-abhinav <abhi.anu003@gmail.com>
2022-07-06 09:03:26 +0530

    flask-restful setup

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask
+from flask_restful import Resource, Api
 from application.config import LocalDevelopmentConfig
 from application.database import db
 
 app = None
+api = None
 
 def create_app():
     app = Flask(__name__, template_folder="templates")
@@ -13,10 +15,11 @@ def create_app():
         print("Starting in local development.")
         app.config.from_object(LocalDevelopmentConfig)
     db.init_app(app)
+    api = Api(app)
     app.app_context().push()
-    return app
+    return app, api
 
-app = create_app()
+app, api = create_app()
 
 from application.controllers import *
 
